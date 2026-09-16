4905c09b0273ef0d88a06fd6a230caaf5813a6c4
Ynizouh <77572689+Ynizouh@users.noreply.github.com>
2026-09-16 11:56:22 +0200

    edit

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -87,7 +87,7 @@ export default function About() {
           <span className="editorial-section-label">PARCOURS</span>
           <div className="editorial-label-bar" />
           <h2 className="editorial-title mb-16">
-            PARCOURS &<br />PROJETS
+            PARCOURS SUPER PPARCCOURS &<br />PROJETS
           </h2>
 
           <div className="border-t border-white/10">
